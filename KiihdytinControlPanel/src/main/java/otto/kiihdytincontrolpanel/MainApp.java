72ead1260b59c45d6bd40ed71c1708a6f454cbde
Ottis1 <otto.kuusela@outlook.com>
2019-07-14 19:25:59 +0300

    Ditch FXML and fix serial communication with Arduino

--- KiihdytinControlPanel/src/main/java/otto/kiihdytincontrolpanel/MainApp.java
+++ KiihdytinControlPanel/src/main/java/otto/kiihdytincontrolpanel/MainApp.java
@@ -1,21 +1,69 @@
 package otto.kiihdytincontrolpanel;
 
 import javafx.application.Application;
-import static javafx.application.Application.launch;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.collections.FXCollections;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-        
-        Scene scene = new Scene(root);
+        SerialLatencyTest test = new SerialLatencyTest();
+
+        ListView list = new ListView(FXCollections.observableArrayList(test.availablePorts()));
+        Label infoLabel = new Label();
+
+        Button testButton = new Button();
+        testButton.setText("Test");
+        testButton.setDisable(true);
+        testButton.setOnAction((event) -> {
+            long result = test.testLatency();
+            infoLabel.setText(Long.toString(result));
+        });
+
+        Button connectButton = new Button();
+        connectButton.setText("Connect");
+        connectButton.setOnAction((event) -> {
+            if (connectButton.getText().equals("Disconnect")) {
+                boolean disconnectSuccessful = test.disconnect();
+                if (disconnectSuccessful) {
+                    testButton.setDisable(true);
+                    connectButton.setText("Connect");
+                    infoLabel.setText("Disconnected");
+                    return;
+                }
+            }
+
+            if (list.getSelectionModel().getSelectedItem() == null) {
+                return;
+            }
+
+            String port = list.getSelectionModel().getSelectedItem().toString();
+            boolean connectionSuccessful = test.connect(port);
+            System.out.println("Connection successful? " + connectionSuccessful);
+
+            if (connectionSuccessful) {
+                infoLabel.setText("Connected to " + port);
+                testButton.setDisable(false);
+                connectButton.setText("Disconnect");
+            }
+        });
+
+        VBox centerControls = new VBox(connectButton, testButton, infoLabel);
+        BorderPane pane = new BorderPane();
+        pane.setPrefSize(600, 400);
+        pane.setLeft(list);
+        pane.setCenter(centerControls);
+
+        Scene scene = new Scene(pane);
         scene.getStylesheets().add("/styles/Styles.css");
-        
+
         stage.setTitle("PC -> Arduino -> PC latency tester");
         stage.setScene(scene);
         stage.show();
@@ -30,7 +78,7 @@ public class MainApp extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        launch(MainApp.class);
     }
 
 }
--- KiihdytinControlPanel/src/main/java/otto/kiihdytincontrolpanel/SerialLatencyTest.java
+++ KiihdytinControlPanel/src/main/java/otto/kiihdytincontrolpanel/SerialLatencyTest.java
@@ -7,52 +7,71 @@ package otto.kiihdytincontrolpanel;
 
 import com.fazecast.jSerialComm.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  *
  * @author ottokuusela
  */
 public class SerialLatencyTest {
-   
+
     private SerialPort port;
-    
+
     public void SerialLatencyTest() {
     }
-    
-    public ArrayList<String> availablePorts() {      
+
+    public ArrayList<String> availablePorts() {
         ArrayList<String> portNames = new ArrayList<>();
-     
+
         for (SerialPort port : SerialPort.getCommPorts()) {
-            portNames.add(port.getPortDescription());
+            portNames.add(port.getSystemPortName());
         }
-        
+
         return portNames;
-    }    
-    public boolean connect(String portDescription) {
-        this.port = SerialPort.getCommPort(portDescription);
-        return this.port.openPort();
     }
-    
+
+    public boolean connect(String portName) {
+        this.port = SerialPort.getCommPort(portName);
+        boolean successful = this.port.openPort();
+        if (successful) {
+            this.port.setComPortParameters(9600, 8, 1, 0);
+        }
+        return successful;
+    }
+
+    public boolean disconnect() {
+        return this.port.closePort();
+    }
+
     public long testLatency() {
         if (this.port == null) {
             return -1;
         }
-        
+
         long start = System.currentTimeMillis();
-        
-        byte[] testData = "5".getBytes();
-        
-        this.port.writeBytes(testData, testData.length);
+
+        byte[] testData = {25, 30};
+
+        int bytesWritten = this.port.writeBytes(testData, testData.length);
+        System.out.println("Successfully sent " + bytesWritten + " bytes:" + Arrays.toString(testData));  
         
         while (this.port.bytesAvailable() == 0) {
             if (System.currentTimeMillis() - start > 1000) {
+                System.out.println("Timed out waiting for data to be echoed back");
                 return -1;
             }
         }
-        
+
+        byte[] readBuffer = new byte[this.port.bytesAvailable()];
+        int numRead = this.port.readBytes(readBuffer, readBuffer.length);
+        System.out.println("Received " + numRead + " bytes:");
+        for (byte b : readBuffer) {
+            System.out.println("Received " + b);
+        }
+
         long end = System.currentTimeMillis();
-        
+
         return end - start;
     }
-    
+
 }
