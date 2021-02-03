5c338d6ee6fb740652a5f5b5d9ecece99cd10a92
Verneri Hirvonen <verneri.hirvonen.2@gmail.com>
2021-02-03 19:57:13 +0200

    Configure firmware for STM32F303RE MCU

--- fw/motherboard/src/main.rs
+++ fw/motherboard/src/main.rs
@@ -1,19 +1,17 @@
-//! Blinks an LED
-//!
-//! This assumes that a LED is connected to pc13 as is the case on the blue pill board.
-//!
-//! Note: Without additional hardware, PC13 should not be used to drive an LED, see page 5.1.2 of
-//! the reference manual for an explanation. This is not an issue on the blue pill.
-
 #![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 
-use panic_halt as _;
+// use panic_halt as _;
+use panic_semihosting as _;
 
 use cortex_m_rt::entry;
+use cortex_m::asm;
 use embedded_hal::digital::v2::OutputPin;
-use stm32f1xx_hal::{pac, prelude::*, delay::Delay};
+use stm32f3xx_hal::{pac, prelude::*, delay::Delay};
+
+// D13 (PA5) is connected to user LED (green)
+// https://www.st.com/resource/en/user_manual/dm00105823-stm32-nucleo64-boards-mb1136-stmicroelectronics.pdf
 
 #[entry]
 fn main() -> ! {
@@ -29,35 +27,41 @@ fn main() -> ! {
     // `clocks`
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-    // Acquire the GPIOC peripheral
-    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
-    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
+    // Acquire GPIO peripherals
+    let mut gpio_a = dp.GPIOA.split(&mut rcc.ahb);
+    let mut _gpio_b = dp.GPIOB.split(&mut rcc.ahb);
+    let mut _gpio_c = dp.GPIOC.split(&mut rcc.ahb);
+    let mut _gpio_d = dp.GPIOD.split(&mut rcc.ahb);
+    let mut _gpio_e = dp.GPIOE.split(&mut rcc.ahb);
+    let mut _gpio_f = dp.GPIOF.split(&mut rcc.ahb);
+    let mut _gpio_g = dp.GPIOG.split(&mut rcc.ahb);
+    let mut _gpio_h = dp.GPIOH.split(&mut rcc.ahb);
 
-    // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
+    // Configure gpio A pin 5 as a push-pull output. The `crh` register is passed to the function
     // in order to configure the port. For pins 0-7, crl should be passed instead.
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh); // PC13 contols dev board LED
-    let mut em = gpiob.pb12.into_push_pull_output(&mut gpiob.crh);  // PB12 controls electromagnet gate
+    let mut led = gpio_a.pa5.into_push_pull_output(&mut gpio_a.moder, &mut gpio_a.otyper).downgrade().downgrade();
+    // let mut em = gpiob.pb12.into_push_pull_output(&mut gpiob.crh);   // PB12 controls electromagnet gate
 
     let mut delay = Delay::new(cp.SYST, clocks);
 
     loop {
         // turn off indicator and wait for a while
-        led.set_high().unwrap();
-        delay.delay_ms(3000_u16);
+        led.set_low().unwrap();
+        asm::delay(3*8_000_000);
 
         // warn about turning on electromagnet by blinking LED
         
         for _ in 0..5 {
-            led.set_low().unwrap();
-            delay.delay_ms(100_u16);
             led.set_high().unwrap();
             delay.delay_ms(100_u16);
+            led.set_low().unwrap();
+            delay.delay_ms(100_u16);
         }
-        led.set_low().unwrap();
+        led.set_high().unwrap();
 
         // toggle electromagnet on for 500ms
-        em.set_high().unwrap();
+        // em.set_high().unwrap();
         delay.delay_ms(500_u16);
-        em.set_low().unwrap();
+        // em.set_low().unwrap();
     }
 }
