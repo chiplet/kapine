cacf3043704e6f9f36a87d9c5f48525e2f277225
Verneri Hirvonen <verneri.hirvonen.2@gmail.com>
2021-05-16 15:23:27 +0300

    Working serial example on motherboard hardware

--- fw/motherboard/src/main.rs
+++ fw/motherboard/src/main.rs
@@ -1,21 +1,15 @@
-//! CDC-ACM serial port example using polling in a busy loop.
+//! Example of transmitting data over serial interface using DMA.
+//! For this to work, the PA9 and PA10 pins must be connected.
 //! Target board: STM32F3DISCOVERY
+
 #![no_std]
 #![no_main]
 
 use panic_semihosting as _;
 
-use stm32f3xx_hal as hal;
-
-use cortex_m::asm::delay;
+use cortex_m::{asm, singleton};
 use cortex_m_rt::entry;
-
-use hal::pac;
-use hal::prelude::*;
-use hal::usb::{Peripheral, UsbBus};
-
-use usb_device::prelude::*;
-use usbd_serial::{SerialPort, USB_CLASS_CDC};
+use stm32f3xx_hal::{pac, prelude::*, serial::Serial};
 
 #[entry]
 fn main() -> ! {
@@ -23,89 +17,52 @@ fn main() -> ! {
 
     let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
-
-    let clocks = rcc
-        .cfgr
-        .use_hse(8u32.MHz())
-        .sysclk(48u32.MHz())
-        .pclk1(24u32.MHz())
-        .pclk2(24u32.MHz())
-        .freeze(&mut flash.acr);
-
-    assert!(clocks.usbclk_valid());
-
-    // Configure the on-board LED (LD10, south red)
-    let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
-    let mut led = gpioe
-        .pe13
-        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-    led.set_low().ok(); // Turn off
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
     let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
 
-    // F3 Discovery board has a pull-up resistor on the D+ line.
-    // Pull the D+ pin down to send a RESET condition to the USB bus.
-    // This forced reset is needed only for development, without it host
-    // will not reset your device when you upload new firmware.
-    let mut usb_dp = gpioa
-        .pa12
-        .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
-    usb_dp.set_low().ok();
-    delay(clocks.sysclk().0 / 100);
+    let pins = (
+        gpioa.pa9.into_af7(&mut gpioa.moder, &mut gpioa.afrh),
+        gpioa.pa10.into_af7(&mut gpioa.moder, &mut gpioa.afrh),
+    );
+    let serial = Serial::usart1(dp.USART1, pins, 9600.bps(), clocks, &mut rcc.apb2);
+    let (tx, rx) = serial.split();
 
-    let usb_dm =
-        gpioa
-            .pa11
-            .into_af14_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrh);
-    let usb_dp = usb_dp.into_af14_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrh);
+    let dma1 = dp.DMA1.split(&mut rcc.ahb);
 
-    let usb = Peripheral {
-        usb: dp.USB,
-        pin_dm: usb_dm,
-        pin_dp: usb_dp,
-    };
-    let usb_bus = UsbBus::new(usb);
+    // the data we are going to send over serial
+    let tx_buf = singleton!(: [u8; 9] = *b"hello DMA").unwrap();
+    // the buffer we are going to receive the transmitted data in
+    let rx_buf = singleton!(: [u8; 9] = [0; 9]).unwrap();
 
-    let mut serial = SerialPort::new(&usb_bus);
+    // DMA channel selection depends on the peripheral:
+    // - USART1: TX = 4, RX = 5
+    // - USART2: TX = 6, RX = 7
+    // - USART3: TX = 3, RX = 2
+    let (tx_channel, rx_channel) = (dma1.ch4, dma1.ch5);
 
-    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
-        .manufacturer("Fake company")
-        .product("Serial port")
-        .serial_number("TEST")
-        .device_class(USB_CLASS_CDC)
-        .build();
+    // start separate DMAs for sending and receiving the data
+    let sending = tx.write_all(tx_buf, tx_channel);
+    let receiving = rx.read_exact(rx_buf, rx_channel);
+
+    // block until all data was transmitted and received
+    let (tx_buf, tx_channel, tx) = sending.wait();
+    let (rx_buf, rx_channel, rx) = receiving.wait();
+
+    assert_eq!(tx_buf, rx_buf);
+
+    // After a transfer is finished its parts can be re-used for another one.
+    tx_buf.copy_from_slice(b"hi again!");
+
+    let sending = tx.write_all(tx_buf, tx_channel);
+    let receiving = rx.read_exact(rx_buf, rx_channel);
+
+    let (tx_buf, ..) = sending.wait();
+    let (rx_buf, ..) = receiving.wait();
+
+    assert_eq!(tx_buf, rx_buf);
 
     loop {
-        if !usb_dev.poll(&mut [&mut serial]) {
-            continue;
-        }
-
-        let mut buf = [0u8; 64];
-
-        match serial.read(&mut buf) {
-            Ok(count) if count > 0 => {
-                led.set_high().ok(); // Turn on
-
-                // Echo back in upper case
-                for c in buf[0..count].iter_mut() {
-                    if 0x61 <= *c && *c <= 0x7a {
-                        *c &= !0x20;
-                    }
-                }
-
-                let mut write_offset = 0;
-                while write_offset < count {
-                    match serial.write(&buf[write_offset..count]) {
-                        Ok(len) if len > 0 => {
-                            write_offset += len;
-                        }
-                        _ => {}
-                    }
-                }
-            }
-            _ => {}
-        }
-
-        led.set_low().ok(); // Turn off
+        asm::wfi();
     }
 }
