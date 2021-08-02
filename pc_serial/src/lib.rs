7767470e38ebceb3310efe1afe361dd3663e00cc
Verneri Hirvonen <verneri.hirvonen.2@gmail.com>
2021-08-02 20:59:00 +0300

    Add WIP write_bytes method

--- pc_serial/src/lib.rs
+++ pc_serial/src/lib.rs
@@ -1,5 +1,6 @@
 mod packet {
 
+    #[derive(Debug)]
     struct Packet {
         sync: u16,
         command: u8,
@@ -9,20 +10,41 @@ mod packet {
     }
 
     impl Packet {
+        // TODO: come up with a better implementation
+        fn write_bytes(&self, buffer: &mut [u8]) -> u32 {
 
-        fn as_bytes() -> {
-            
+            let mut i = 0;
+            buffer[i] = self.sync as u8; i = i + 1;
+            buffer[i] = (self.sync >> 8) as u8; i = i + 1;
+
+            (self.length + 6).into()
         }
 
-        fn new (command: u8, payload: &[u8]) -> Self {
-            Packet {
+        fn compute_checksum(&mut self) {
+            unimplemented!("TODO: Implement checksum computation");
+        }
+
+        fn new(command: u8, payload: [u8; 255]) -> Self {
+            let payload_length = payload.len();
+            assert!(payload_length <= 255);
+
+            let mut packet = Packet {
                 sync: 0x55AA,
                 command: command,
-                length: payload.len(),
+                length: payload_length as u8,
                 payload: payload,
-                checksum:
-            }
+                checksum: 0u16, // populated by compute_checksum
+            };
+            packet.compute_checksum();
+            packet
         }
     }
+}
 
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn sample_test() {
+        assert_eq!(2 + 2, 4);
+    }
 }
