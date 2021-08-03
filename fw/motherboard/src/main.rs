a1428ab666f7c782c7e9fc66d88a20141df970b9
Verneri Hirvonen <verneri.hirvonen.2@gmail.com>
2021-08-03 22:01:10 +0300

    WIP UART receiver with RTIC and interrupts

--- fw/motherboard/src/main.rs
+++ fw/motherboard/src/main.rs
@@ -1,68 +1,121 @@
-//! Example of transmitting data over serial interface using DMA.
-//! For this to work, the PA9 and PA10 pins must be connected.
-//! Target board: STM32F3DISCOVERY
-
-#![no_std]
+#![deny(unsafe_code)]
 #![no_main]
+#![no_std]
 
+use cortex_m_semihosting::{debug, hprint, hprintln};
 use panic_semihosting as _;
+use rtic::app;
 
-use cortex_m::{asm, singleton};
-use cortex_m_rt::entry;
-use stm32f3xx_hal::{pac, prelude::*, serial::Serial};
+use stm32f3xx_hal::pac as pac;
 
-#[entry]
-fn main() -> ! {
-    let dp = pac::Peripherals::take().unwrap();
+use stm32f3xx_hal::{
+    prelude::*,
+    serial::{
+        Serial,
+        Tx,
+        Rx,
+        Event::Rxne
+    }
+};
 
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+// interrupts
+use pac::Interrupt::{
+    self,
+    USART1_EXTI25
+};
 
-    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
+use kapine_packet::Packet;
 
-    let pins = (
-        gpioa.pa9.into_af7(&mut gpioa.moder, &mut gpioa.afrh),
-        gpioa.pa10.into_af7(&mut gpioa.moder, &mut gpioa.afrh),
-    );
-    let serial = Serial::usart1(dp.USART1, pins, 9600.bps(), clocks, &mut rcc.apb2);
-    let (tx, rx) = serial.split();
+const RX_BUF_SIZE: usize = 10;
 
-    let dma1 = dp.DMA1.split(&mut rcc.ahb);
+pub struct Buffer {
+    index: usize,
+    pub data: [u8; RX_BUF_SIZE],
+}
 
-    // the data we are going to send over serial
-    let tx_buf = singleton!(: [u8; 9] = *b"hello DMA").unwrap();
-    // the buffer we are going to receive the transmitted data in
-    let rx_buf = singleton!(: [u8; 9] = [0; 9]).unwrap();
+impl Buffer {
+    const fn new() -> Self {
+        Buffer {
+            index: 0,
+            data: [0; RX_BUF_SIZE],
+        }
+    }
 
-    // DMA channel selection depends on the peripheral:
-    // - USART1: TX = 4, RX = 5
-    // - USART2: TX = 6, RX = 7
-    // - USART3: TX = 3, RX = 2
-    let (tx_channel, rx_channel) = (dma1.ch4, dma1.ch5);
-
-    // start separate DMAs for sending and receiving the data
-    let sending = tx.write_all(tx_buf, tx_channel);
-    let receiving = rx.read_exact(rx_buf, rx_channel);
-
-    // block until all data was transmitted and received
-    let (tx_buf, tx_channel, tx) = sending.wait();
-    let (rx_buf, rx_channel, rx) = receiving.wait();
-
-    assert_eq!(tx_buf, rx_buf);
-
-    // After a transfer is finished its parts can be re-used for another one.
-    tx_buf.copy_from_slice(b"hi again!");
-
-    let sending = tx.write_all(tx_buf, tx_channel);
-    let receiving = rx.read_exact(rx_buf, rx_channel);
-
-    let (tx_buf, ..) = sending.wait();
-    let (rx_buf, ..) = receiving.wait();
-
-    assert_eq!(tx_buf, rx_buf);
-
-    loop {
-        asm::wfi();
+    // TODO: add error handling
+    fn push(&mut self, data: u8) {
+        self.data[self.index] = data;
+        self.index += 1;
     }
 }
+
+#[rtic::app(device = stm32f3xx_hal::pac, peripherals = true)]
+const APP: () = {
+    struct Resources {
+        TX: Tx<pac::USART1>,
+        RX: Rx<pac::USART1>,
+        #[init(Buffer::new())]
+        RX_BUF: Buffer,
+    }
+
+    #[init]
+    fn init(cx: init::Context) -> init::LateResources {
+        hprintln!("init").unwrap();
+
+        let _core = cx.core;
+        let device = cx.device;
+
+        let mut flash = device.FLASH.constrain();
+        let mut rcc = device.RCC.constrain();
+
+        // configure clocks
+        let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+        let mut gpioa = device.GPIOA.split(&mut rcc.ahb);
+
+        let pins = (
+            gpioa.pa9.into_af7(&mut gpioa.moder, &mut gpioa.afrh),
+            gpioa.pa10.into_af7(&mut gpioa.moder, &mut gpioa.afrh),
+        );
+        let mut serial = Serial::usart1(device.USART1, pins, 9600.bps(), clocks, &mut rcc.apb2);
+        serial.listen(stm32f3xx_hal::serial::Event::Rxne);
+
+        let (tx, rx) = serial.split();
+
+        rtic::pend(Interrupt::USART1_EXTI25);
+
+        // let RX_BUF: 
+
+        init::LateResources {
+            TX: tx,
+            RX: rx,
+        }
+    }
+
+    #[idle( resources = [RX_BUF] )]
+    fn idle(mut cx: idle::Context) -> ! {
+
+
+        // rtic::pend(Interrupt::USART1_EXTI25);
+
+        hprintln!("idle").unwrap();
+
+        loop {
+            cx.resources.RX_BUF.lock(|RX_BUF| {
+                if (RX_BUF.data[0] != 0) {
+                    hprint!("{:?} ", RX_BUF.data);
+                }
+            });
+        }
+    }
+
+    #[task(binds = USART1_EXTI25, priority = 1, resources=[RX,RX_BUF,TX])]
+    fn usart1(cx: usart1::Context) {
+
+        match cx.resources.RX.read() {
+            Ok(c) => {
+                cx.resources.RX_BUF.push(c);
+            }
+            Err(e) => (),
+        };
+    }
+};
